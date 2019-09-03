31e4fb3de41489ce6b2fad9cda75db65923f00b2
Pruthvi_soni <pruthvisoni7@gmail.com>
2019-09-03 22:31:36 +0530

    MotoFit 1st Stable Login & register Module

---
+++ app/src/main/java/com/test/motofit_temp/test_1/login.java
@@ -0,0 +1,100 @@
+package com.test.motofit_temp.test_1;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class login extends AppCompatActivity
+{
+    EditText pwd,mail;
+    Button loginup;
+    TextView signup;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        mAuth = FirebaseAuth.getInstance();
+        loginup = (Button) findViewById(R.id.but1);
+        mail= (EditText) findViewById(R.id.email);
+        pwd = (EditText) findViewById(R.id.password);
+        signup = (TextView) findViewById(R.id.tv1);
+        loginup.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mail.getText().toString().trim();
+                String password = pwd.getText().toString().trim();
+                if (email.isEmpty()) {
+                    mail.setError("Email Required!");
+                    mail.requestFocus();
+                    return;
+                }
+                if (password.isEmpty()) {
+                    pwd.setError("Password Required!");
+                    pwd.requestFocus();
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    mail.setError("Please Enter Valid Email!");
+                    mail.requestFocus();
+                    return;
+                }
+                if (pwd.length() < 6) {
+                    pwd.setError("Minimum Length of Password be 6");
+                    pwd.requestFocus();
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent it = new Intent(login.this, home.class);
+                            it.addFlags(it.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(it);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        signup.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(login.this,signup.class);
+                startActivity(it);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/test/motofit_temp/test_1/signup.java
@@ -0,0 +1,94 @@
+package com.test.motofit_temp.test_1;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+public class signup extends AppCompatActivity {
+    EditText mail, pass;
+    //,mophone,usrusr;
+    Button sup;
+    TextView lin;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+        mail = (EditText) findViewById(R.id.email);
+        pass = (EditText) findViewById(R.id.pwd);
+        sup = (Button) findViewById(R.id.signup);
+        lin = (TextView) findViewById(R.id.tv2);
+        mAuth = FirebaseAuth.getInstance();
+        //Use Later
+        //usrusr = (EditText) findViewById(R.id.username);
+        //mophone = (EditText) findViewById(R.id.mobphone);
+        //
+        sup.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mail.getText().toString().trim();
+                String password = pass.getText().toString().trim();
+                if (email.isEmpty()) {
+                    mail.setError("Email Required!");
+                    mail.requestFocus();
+                    return;
+                }
+                if (password.isEmpty()) {
+                    pass.setError("Password Required!");
+                    pass.requestFocus();
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    mail.setError("Please Enter Valid Email!");
+                    mail.requestFocus();
+                    return;
+                }
+                if (password.length() < 6) {
+                    pass.setError("Minimum Length of Password be 6");
+                    pass.requestFocus();
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "User Registered", Toast.LENGTH_LONG).show();
+                            Intent it = new Intent(signup.this,login.class);
+                            startActivity(it);
+                        } else {
+                            if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                                Toast.makeText(getApplicationContext(),"You Are Already Registered",Toast.LENGTH_LONG).show();
+                            }else{
+                                Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
+            }
+
+        });
+        lin.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(signup.this, login.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
