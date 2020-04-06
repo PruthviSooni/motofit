40fc7a0967b16878b5aad3916aa995c2ae26748e
PruthviSooni <pruthvisoni7@gmail.com>
2020-04-06 15:27:55 +0530

    UI Adjustment in hole app (#12)

--- app/src/main/java/com/motofit/app/Adapters/service_adapter.java
+++ app/src/main/java/com/motofit/app/Adapters/service_adapter.java
@@ -31,18 +31,13 @@ public class service_adapter extends ArrayAdapter<Services> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         LayoutInflater inflater = LayoutInflater.from(context);
         @SuppressLint("InflateParams") View list_view_Item = inflater.inflate(R.layout.service_list_layout, null, true);
-        TextView location = list_view_Item.findViewById(R.id.usr_location);
         TextView time = list_view_Item.findViewById(R.id.time);
         final TextView service_type = list_view_Item.findViewById(R.id.service_type);
-        TextView notes = list_view_Item.findViewById(R.id.usr_notes);
-        TextView odometer = list_view_Item.findViewById(R.id.usr_odometer);
 
         final Services services = serviceList.get(position);
-        location.setText(services.location);
+
         time.setText(services.time);
         service_type.setText(services.type_service);
-        notes.setText(services.notes);
-        odometer.setText(services.odometer);
 
 
         return list_view_Item;
